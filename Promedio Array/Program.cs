7cdc81c6bff37292173fce2235eebe44b9329b52
theboxpe <perezregaladogabriel7@gmail.com>
2024-01-04 23:33:51 -0400

    first commit

---
+++ Promedio Array/Program.cs
@@ -0,0 +1,43 @@
+﻿// un programa en el que realizaras un array que contenga 3 valores los cuales luego van a ser utilizados para sacar el promedio de estos valores, luego que diga la raiz de dicho promedio, utilizando funciones.
+
+class program
+{
+    static int [] numero = new int[3];
+    
+    static void Main(string[] args)
+    {
+        num();
+        
+    }
+
+    static void num()
+    {
+        int promedio;
+        double raiz;
+
+        Console.WriteLine("Ingrese 3 valores diferentes");
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Valor {i + 1}: ");
+            numero[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        promedio = numero[0] + numero[1] + numero[2]/3;
+        Console.WriteLine("El promedio de los valores es: ");
+        Console.WriteLine(promedio);
+
+        raiz = Math.Sqrt(promedio);
+        Console.WriteLine("la raiz del promedio dado es:");
+        Console.WriteLine(raiz);
+
+    }
+
+   
+    
+
+
+}
+
+
+
---
+++ Suma Array/Program.cs
@@ -0,0 +1,30 @@
+﻿//hacer una funcion que duelva la suma de un arreglo o array.
+
+class program
+{
+    static int [] num = new int[5];
+    
+    static void Main(string []args)
+    { 
+       Suma(); 
+    }
+
+    static void Suma()
+    {
+        Console.WriteLine("Ingrese los valores a sumar:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            num[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int suma = num.Sum();
+
+        Console.WriteLine("La suma de los valores es: " + suma);
+
+        Console.ReadKey();
+    }
+
+
+}
+
